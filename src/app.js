09a202f1a9e1fda0835d804b811898d42184f935
Hernán <hernanmf2001@gmail.com>
2024-06-03 20:06:06 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomnumber = Math.random() * 10;
+  document.querySelector("#theexcuse").innerHTML = generateExcuse();
+  console.log("Hello Rigo from the console! ");
+};
+let generateExcuse = () => {
+  let who = ["My dog", "My grandma", "A policeman", "My neighbour"];
+  let action = ["ate", "stole", "broke", "peed"];
+  let what = ["my homework", "the car", "my phone"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
